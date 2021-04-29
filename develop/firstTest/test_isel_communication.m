78d47b6415f4d5c13e1858caeeb7eb506ac33557
janPeterGit <jan.balmes@gmail.com>
2021-04-29 11:20:58 +0200

    basic example works

--- develop/firstTest/test_isel_communication.m
+++ develop/firstTest/test_isel_communication.m
@@ -12,50 +12,71 @@ clear all
 close all
 
 
-COM_port = 'COM1';
-t_pause = 0.5; % seconds
+
+% pal pc input
+% #axis X;
+% 
+% #steps 800; % not used in terminal
+% #elev 2.5 % not used in terminal
+% #ref_speed 5000
+% #units mm; % not used in terminal
+% 
+% #input
+% 
+% reference x;
+% moverel 100(5000),0(3000),0(3000);
+% 
+% 
+% stop.
+% 
+% #start
 
 % Terminal output
-% @03              2 Achsen setzen
-% @0d1000,1000     ref speed setzen
-% @0R3             xy referenzieren
+% @01              1 Achsen setzen
+% @0d5000          ref speed setzen
 % @0i              #input Befehl
-% m0,3000,0,5000   absolutfahrt posx,Geschw1, posy,Geschw2
+% 71             reference x
+% 032000,5000      moverel 
 % 9                stop. befehl
 % @0S              #start Befehle
 
+% COM settings
+COM_port = 'COM3';
+baudrate = 19200;
 
+t_pause = 0.1; % seconds
 
 % open serial port
-serial_COM = serial(COM_port,'Baudrate',38400,'Parity','none','Databits',8,'StopBits',1);
-fopen(serial_COM);
-status_COM = get(serial_COM, 'Status');     % fragt den Status ab
-display(status_COM);
-set(serial_COM,'Terminator','CR/LF');       % unbedingt notwendig, für ECHO Antwort
+device = serialport(COM_port,baudrate);
+configureTerminator(device,"CR/LF")      % unbedingt notwendig, für ECHO Antwort
+pause(t_pause);
+device.Timeout = 10; % default 10
+pause(t_pause);
+% resp = writeread(device,"*IDN?")
 pause(t_pause);
 
 % send commands
-fprintf(serial_COM,'@03');                  % 2 Achsen aktivieren
+disp('send commands')
+writeline(device,"@01");                  % 1 Achsen aktivieren
 pause(t_pause);
-fprintf(serial_COM,'@0d1000,1000');         % Ref speed setzen
+writeline(device,"@0d5000");              % Ref speed setzen
 pause(t_pause);
-fprintf(serial_COM,'@0R3');                 % 2 Achsen referenzieren
+writeline(device,"@0i");                  % #input Befehl
 pause(t_pause);
-fprintf(serial_COM,'@0i');                  % #input Befehl
+writeline(device,"71");                   % 1 Achsen referenzieren
 pause(t_pause);
-fprintf(serial_COM,'m0,3000,0,3000');       % fahre an abs Pos 0,0 mit Geschw. 3000
+% writeline(device,'032000,5000');        % fahre an abs Pos 0,0 mit Geschw. 3000
+% pause(t_pause);
+writeline(device,"9");                    % stop. Befehl
 pause(t_pause);
-fprintf(serial_COM,'9');                    % stop. Befehl
-pause(t_pause);
-fprintf(serial_COM,'@0S');                  % #start Befehl
+writeline(device,"@0S");                  % #start Befehl
+disp('commands sent')
+disp('5s pause')
 pause(5);
  
 % close serial port
-fclose(serial_COM);
-status_COM = get(serial_COM, 'Status');
-display(status_COM);
-delete(serial_COM);
-clear serial_COM;
+% delete(device);
+clear device;
 
 
 
