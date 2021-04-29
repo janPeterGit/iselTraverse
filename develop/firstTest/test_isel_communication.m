83ee2c319a668856aaeb9fbfd44ece1bb912f016
janPeterGit <jan.balmes@gmail.com>
2021-04-29 11:58:03 +0200

    update

--- develop/firstTest/test_isel_communication.m
+++ develop/firstTest/test_isel_communication.m
@@ -11,32 +11,24 @@ clc
 clear all
 close all
 
-
-
 % pal pc input
-% #axis X;
-% 
-% #steps 800; % not used in terminal
-% #elev 2.5 % not used in terminal
+% #axis X; 
+% #steps 800; % nicht im terminal output
+% #elev 2.5 % nicht im terminal output
 % #ref_speed 5000
-% #units mm; % not used in terminal
-% 
+% #units mm; % nicht im terminal output
 % #input
-% 
 % reference x;
-% moverel 100(5000),0(3000),0(3000);
-% 
-% 
+% moverel 10(5000),0(3000),0(3000);
 % stop.
-% 
 % #start
 
-% Terminal output
+% pal pc terminal output
 % @01              1 Achsen setzen
 % @0d5000          ref speed setzen
 % @0i              #input Befehl
 % 71             reference x
-% 032000,5000      moverel 
+% 03200,5000      moverel 
 % 9                stop. befehl
 % @0S              #start Befehle
 
@@ -44,15 +36,13 @@ close all
 COM_port = 'COM3';
 baudrate = 19200;
 
-t_pause = 0.1; % seconds
+t_pause = 0.5; % seconds @Mario wozu ist die Pause? Funktioniert auch ohne
 
-% open serial port
+% connect serial port
 device = serialport(COM_port,baudrate);
 configureTerminator(device,"CR/LF")      % unbedingt notwendig, für ECHO Antwort
 pause(t_pause);
 device.Timeout = 10; % default 10
-pause(t_pause);
-% resp = writeread(device,"*IDN?")
 pause(t_pause);
 
 % send commands
@@ -65,7 +55,7 @@ writeline(device,"@0i");                  % #input Befehl
 pause(t_pause);
 writeline(device,"71");                   % 1 Achsen referenzieren
 pause(t_pause);
-% writeline(device,'032000,5000');        % fahre an abs Pos 0,0 mit Geschw. 3000
+% writeline(device,'03200,5000');        % move relative 100m (steps 800, elev 2.5mm)
 % pause(t_pause);
 writeline(device,"9");                    % stop. Befehl
 pause(t_pause);
@@ -75,8 +65,8 @@ disp('5s pause')
 pause(5);
  
 % close serial port
-% delete(device);
-clear device;
+delete(device);
+clear variables
 
 
 
